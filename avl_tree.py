f23f93561d99e5094bc0a71c5c6ba546fbe16798
Adam Kraś <adam.kras.stud@pw.edu.pl>
2023-03-30 22:45:44 +0200

    Intro AVL implementation

--- avl_tree.py
+++ avl_tree.py
@@ -1,6 +1,88 @@
-class AVL_tree:
-    def __init__(self):
-        self.root = None
-        self.size = 0
+class tree_node:
+    def __init__(self, key):
+        self.key = key
         self.left = None
         self.right = None
+        self.height = 1
+
+
+class AVL_Tree:
+    def insert_node(self, root, key):
+        if not root:
+            return TreeNode(key)
+        elif key < root.key:
+            root.left = self.insert_node(root.left, key)
+        else:
+            root.right = self.insert_node(root.right, key)
+
+        root.height = 1 + max(self.get_height(root.left), self.get_height(root.right))
+
+        balanceFactor = self.get_balance(root)
+        if balanceFactor > 1:
+            if key < root.left.key:
+                return self.right_rotate(root)
+            else:
+                root.left = self.left_rotate(root.left)
+                return self.right_rotate(root)
+
+        if balanceFactor < -1:
+            if key > root.right.key:
+                return self.left_rotate(root)
+            else:
+                root.right = self.right_rotate(root.right)
+                return self.left_rotate(root)
+
+        return root
+
+    def left_rotate(self, z):
+        y = z.right
+        T2 = y.left
+        y.left = z
+        z.right = T2
+        z.height = 1 + max(self.get_height(z.left), self.get_height(z.right))
+        y.height = 1 + max(self.get_height(y.left), self.get_height(y.right))
+        return y
+
+    def right_rotate(self, z):
+        y = z.left
+        T3 = y.right
+        y.right = z
+        z.left = T3
+        z.height = 1 + max(self.get_height(z.left), self.get_height(z.right))
+        y.height = 1 + max(self.get_height(y.left), self.get_height(y.right))
+        return y
+
+    def get_height(self, root):
+        if not root:
+            return 0
+        return root.height
+
+    def get_balance(self, root):
+        if not root:
+            return 0
+        return self.get_height(root.left) - self.get_height(root.right)
+
+    def get_min_value_node(self, root):
+        if root is None or root.left is None:
+            return root
+        return self.get_min_value_node(root.left)
+
+    def pre_order(self, root):
+        if not root:
+            return
+        print("{0} ".format(root.key), end="")
+        self.pre_order(root.left)
+        self.pre_order(root.right)
+
+    def print_helper(self, current_pointer, indent, last):
+        if current_pointer != None:
+            sys.stdout.write(indent)
+            if last:
+                sys.stdout.write("R----")
+                indent += "     "
+            else:
+                sys.stdout.write("L----")
+                indent += "|    "
+            print(current_pointer.key)
+            self.print_helper(current_pointer.left, indent, False)
+            self.print_helper(current_pointer.right, indent, True)
