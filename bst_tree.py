ac1ef700a83322e179909b343fc39f1ee4915191
Krzysztof Król <krzysztof.krol.stud@pw.edu.pl>
2023-04-02 13:08:17 +0200

    Not entirely working insert method and print method

--- bst_tree.py
+++ bst_tree.py
@@ -24,9 +24,17 @@ class BST_tree:
         if not root:
             root = Node(key)
         elif key < root.key:
-            self.insert(root.left, key)
+            if not root.left:
+                root.left = Node(key)
+                root.left.parent = root
+            else:
+                self.insert(root.left, key)
         elif key > root.key:
-            self.insert(root.right, key)
+            if not root.right:
+                root.right = Node(key)
+                root.right.parent = root
+            else:
+                self.insert(root.right, key)
 
     def remove(self, root, key):
         if not root:
@@ -53,3 +61,28 @@ class BST_tree:
         while root.left:
             root = root.left
         return root
+
+    def print_tree(self, root):
+        if not root:
+            return None
+        print(root.key)
+        self.print_tree(root.left)
+        self.print_tree(root.right)
+
+
+def main():
+    root = Node(5)
+    tree = BST_tree(root)
+    tree.insert(root, 3)
+    tree.insert(root, 2)
+    tree.insert(root, 4)
+    tree.insert(root, 7)
+    tree.insert(root, 6)
+    tree.insert(root, 8)
+    print(tree.search(root, 6))
+    tree.remove(root, 6)
+    print(tree.search(root, 6))
+
+
+if __name__ == "__main__":
+    main()
