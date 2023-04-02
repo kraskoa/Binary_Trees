842496ce9d2e82c1505bab5b0a749d752c177a6e
Krzysztof Król <krzysztof.krol.stud@pw.edu.pl>
2023-04-02 12:55:24 +0200

    Separated Class Node and BST_tree Node. Search, Insert, Delete, and Print functions are implemented.

--- bst_tree.py
+++ bst_tree.py
@@ -1,35 +1,55 @@
-class BST_tree:
-    def __inti__(self, root):
-        self.root = root
+class Node:
+    def __init__(self, key):
+        self.key = key
         self.left = None
         self.right = None
+        self.parent = None
 
-    def search(self, key):
-        if self.root == key:
-            return True
-        elif self.root > key:
-            if self.left:
-                return self.left.search(key)
-            else:
-                return False
-        else:
-            if self.right:
-                return self.right.search(key)
-            else:
-                return False
 
-    def insert(self, key):
-        if self.root == key:
-            return False
-        elif self.root > key:
-            if self.left:
-                return self.left.insert(key)
-            else:
-                self.left = BST_tree(key)
-                return True
+class BST_tree:
+    def __init__(self, root: Node):
+        self.root = root
+
+    def search(self, root, key):
+        if not root:
+            return None
+        elif root.key == key:
+            return key
+        elif key < root.key:
+            return self.search(root.left, key)
+        elif key > root.key:
+            return self.search(root.right, key)
+
+    def insert(self, root, key):
+        if not root:
+            root = Node(key)
+        elif key < root.key:
+            self.insert(root.left, key)
+        elif key > root.key:
+            self.insert(root.right, key)
+
+    def remove(self, root, key):
+        if not root:
+            return None
+        elif key < root.key:
+            self.remove(root.left, key)
+        elif key > root.key:
+            self.remove(root.right, key)
         else:
-            if self.right:
-                return self.right.insert(key)
+            if root.left and root.right:
+                min_node = self.find_min(root.right)
+                root.key = min_node.key
+                self.remove(root.right, min_node.key)
+            elif root.left:
+                root = root.left
+            elif root.right:
+                root = root.right
             else:
-                self.right = BST_tree(key)
-                return True
+                root = None
+
+    def find_min(self, root):
+        if not root:
+            return None
+        while root.left:
+            root = root.left
+        return root
